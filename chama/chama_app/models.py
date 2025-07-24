765cd9c16b98ec1668ecb27c083e746c7a025e88
Mikee100 <99830445+Mikee100@users.noreply.github.com>
2025-07-25 00:30:10 +0300

    updated the backend

--- chama/chama_app/models.py
+++ chama/chama_app/models.py
@@ -42,6 +42,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     USERNAME_FIELD = 'email'  # Fixed typo from USERNAME_FIELS
     REQUIRED_FIELDS = []
 
+
     def __str__(self):
         
         return self.email
